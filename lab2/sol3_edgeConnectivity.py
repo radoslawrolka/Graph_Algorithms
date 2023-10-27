589343ec42b70ebf4662c0f3545703eb6199b277
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-10-27 14:08:34 +0200

    changed assumptons of the task

--- lab2/sol3_edgeConnectivity.py
+++ lab2/sol3_edgeConnectivity.py
@@ -2,12 +2,13 @@ from test_sol3 import runtests
 from queue import Queue
 _sentinel = object()
 
+# O(V * E^2*V)
 
 def toNeighbour(V, E):
     graph = [[] for _ in range(V)]
-    for (x, y, c) in E:
+    for (x, y, _) in E:
         graph[x-1].append([y-1, 1])
-        graph[y-1].append([x-1, 0])
+        graph[y-1].append([x-1, 1])
     return graph
 
 
@@ -15,11 +16,11 @@ def maxFlowBFS(V, E, s=0, t=_sentinel):
     if t is _sentinel:
         t = V - 1
     graph = toNeighbour(V, E)
-    result = 0
+    flow = 0
 
     def augumentingPath():
         nonlocal V, graph, s, t
-        taken = [True] + [False] * (V-1)
+        size = [True] + [False] * (V-1)
         parent = [None] * V
 
         que = Queue()
@@ -36,16 +37,15 @@ def maxFlowBFS(V, E, s=0, t=_sentinel):
                 path.reverse()
                 return path
             for neighbour, capacity in graph[vertex]:
-                minimum = taken[vertex] and capacity
-                if taken[neighbour] < minimum:
-                    taken[neighbour] = minimum
+                if not size[neighbour] and capacity:
+                    size[neighbour] = True
                     parent[neighbour] = vertex
                     que.put(neighbour)
         return None
 
     path = augumentingPath()
-    while path is not None:
-        result += 1
+    while path:
+        flow += 1
         for i in range(len(path) - 1):
             u, v = path[i], path[i + 1]
             for j, (neighbour, _) in enumerate(graph[u]):
@@ -57,8 +57,18 @@ def maxFlowBFS(V, E, s=0, t=_sentinel):
                     graph[v][j][1] = 1
                     break
         path = augumentingPath()
-    return result
+    return flow
 
+def otocz(V, E):
+    mini = float("inf")
+    for i in range(1, V):
+        mini = min(mini, maxFlowBFS(V, E, 0, i))
+    return mini
+
+
+def Optimal(V, E):
+    return len(min(toNeighbour(V, E), key=lambda x: len(x)))
 
 if __name__ == "__main__":
-    runtests(maxFlowBFS)
+    runtests(optimal)
+    #runtests(otocz)
