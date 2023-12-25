83b0fbb6f9e216d0c6c69bb07ddace24057b7e6a
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-25 16:26:27 +0100

    project1 tests framework

---
+++ project1/example.py
@@ -0,0 +1,9 @@
+from data import runtests
+
+def my_solve(N, streets):
+    print(f"Place: {N}, ulice: {len(streets)}")
+    for a, b, t in streets:
+        pass
+    return 0, 0
+
+runtests(my_solve)
