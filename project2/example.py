051b6d5670809233a6019f9fbc2df91175a3c444
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-01-31 22:24:02 +0100

    initial dir for project2

---
+++ project2/example.py
@@ -0,0 +1,11 @@
+from data import runtests
+
+def my_solve(N, entrance, corridors, path):
+    print(f"Komnaty: {N}, korytarze: {len(corridors)}")
+
+    for c in path.split(" "):
+        ...
+
+    return False
+
+runtests(my_solve)
